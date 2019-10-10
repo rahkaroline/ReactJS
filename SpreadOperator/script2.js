970cb87c0fee52baa0be760a460511aece8c9139
Rayane Karoline Alves de Jesus <rayanek@kyros.com.br>
2019-10-10 11:47:00 -0300

    ReactJS

---
+++ SpreadOperator/script2.js
@@ -0,0 +1,12 @@
+function minhaLista(...nomes) {
+    console.log(nomes)
+}
+
+minhaLista("Matheus", "Lucas", "Joao", "Divina")
+
+
+function meusNumeros(...numeros){
+    console.log(numeros)
+}
+
+meusNumeros(1,2,3,4,5,6,7)

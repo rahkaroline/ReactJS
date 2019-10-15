21a934504d2122e719efa8f194420784a91ea20d
Rayane Karoline Alves de Jesus <rayanek@kyros.com.br>
2019-10-15 15:29:04 -0300

    ReactJS

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,41 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            nome: 'Matheus',
-            contador: 0
-        };
-
-        this.aumentar = this.aumentar.bind(this);
-        this.diminuir = this.diminuir.bind(this);
-    }
-
-    aumentar(){
-        let state = this.state;
-        state.contador +=1;
-        state.nome = 'José';
-        this.setState(state)
-    }
-
-    diminuir(){
-        let state = this.state;
-        state.contador -=1;
-        this.setState(state)
-    }
 
     render(){
         return(
             <div>
-                <h1>Contador</h1>
-                {this.state.nome}
-                <h3>
-                <button onClick={this.diminuir}>-</button>
-                {this.state.contador}
-                <button onClick={this.aumentar}>+</button>
-                </h3>
+                <Membro nome="Visitante"/>
             </div>
         );
     }

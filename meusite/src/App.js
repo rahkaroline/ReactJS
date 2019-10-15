dba81a909f4a1b9eb84f3ad4232bd77ede30d082
Rayane Karoline Alves de Jesus <rayanek@kyros.com.br>
2019-10-15 09:58:21 -0300

    ReactJS

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,34 +1,43 @@
 import React, {Component} from 'react';
 
-class Equipe extends Component{
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            nome: 'Matheus',
+            contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+        let state = this.state;
+        state.contador +=1;
+        this.setState(state)
+    }
+
+    diminuir(){
+        let state = this.state;
+        state.contador -=1;
+        this.setState(state)
+    }
+
     render(){
         return(
             <div>
-                <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
+                <h1>Contador</h1>
+
+                <h3>
+                <button onClick={this.diminuir}>-</button>
+                {this.state.contador}
+                <button onClick={this.aumentar}>+</button>
+                </h3>
             </div>
         );
     }
-}
-
-class Sobre extends Component{
-    render(){
-        return(
-            <div>
-            <h2>Olá sou o(a){this.props.nome}</h2>
-            <h3>Cargo: {this.props.cargo}</h3>
-            <h3>Idade: {this.props.idade}</h3>
-            </div>
-        );
-    }
-}
-
-function App() {
-    return(
-        <div>
-            <h1>Conheça nossa equipe: </h1>
-            <Equipe nome="Matheus" cargo="Programador" idade="24"/>
-        </div>
-    );
 }
 
 export default App;
